84a3d4f494307cc84ad8c14a113a6142291ebbc6
maiduydien <maiduydien@gmail.com>
2021-07-29 11:25:33 +0700

    5

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,18 @@
-let getHomePage =(req,res)=>{
-    return res.render('homePage.ejs')
+import db from './../models/index'
+
+let getHomePage =async(req,res)=>{
+    try{
+            let data= await db.User.findAll();
+            // console.log('----------')
+            // console.log(data)
+
+            return res.render('homePage.ejs',{data:JSON.stringify(data)})
+        }
+        catch (e)
+        {
+            console.log(e)
+        }
+    
 }
 
 module.exports = {
